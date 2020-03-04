3b7cb2cb9f2d3c9492cd8708c46a328eed521475
Baiqi <zhangshuo023@163.com>
2020-03-04 20:33:19 +0800

    simple echo program

---
+++ echo.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include <sys/stat.h>
+ 
+#include <stdlib.h>
+ 
+int
+main(int argc, char *argv[])
+{
+ 
+  int i;
+ 
+  for(i = 1; i < argc; i++)
+    printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
+  exit();
+ 
+}
